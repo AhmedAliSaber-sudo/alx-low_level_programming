e3d57ef575157e89059f446978655fc1d6579b0b
root <root@Revision-PC>
2023-03-21 10:03:13 +0200

    c file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - the starting point.
+ *
+ * Return: always 0 (success).
+ */
+
+int main (void)
+{
+	char lower_alpha = 'a';
+	char capital_alpha = 'A';
+
+	while (lower_alpha >= 'z')
+	{
+		putchar(lower_alpha);
+		lower_alpha++;
+	}
+	while (capital_alpha >= 'Z')
+	{
+		putchar(capital_alpha);
+		capital_alpha++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
