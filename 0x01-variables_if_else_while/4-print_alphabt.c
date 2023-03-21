6271f2cdd7ec44785f2430e9ca1b385ee0c1a41f
root <root@Revision-PC>
2023-03-21 10:21:22 +0200

    c file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - the starting point.
+ *
+ * Return: always 0 (success).
+ */
+
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		if (alpha != 'e' && alpha != 'q')
+		{
+			putchar(alpha);
+			alpha++;
+		}
+		else
+		{
+			continue;
+		}
+	}
+	
+	putchar('\n');
+	return (0);
+}
