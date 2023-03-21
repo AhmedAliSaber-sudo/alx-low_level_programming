01b60b1b350048c2de625bee79ee27fe62dee6e0
root <root@Revision-PC>
2023-03-21 10:07:24 +0200

    c file

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,12 +11,12 @@ int main (void)
 	char lower_alpha = 'a';
 	char capital_alpha = 'A';
 
-	while (lower_alpha >= 'z')
+	while (lower_alpha <= 'z')
 	{
 		putchar(lower_alpha);
 		lower_alpha++;
 	}
-	while (capital_alpha >= 'Z')
+	while (capital_alpha <= 'Z')
 	{
 		putchar(capital_alpha);
 		capital_alpha++;
