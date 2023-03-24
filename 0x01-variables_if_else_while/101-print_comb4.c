28f84c605b573c6b41d88cc798b268e0ee5331cd
ahmed <ahmed@hunter>
2023-03-24 10:37:43 +0200

    solved

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 	int i, j, k;
-	
+
 	for (i = 0; i <= 7; i++)
 	{
 		for (j = i + 1; j <= 8; j++)
@@ -19,7 +19,6 @@ int main(void)
 				putchar(i + '0');
 				putchar(j + '0');
 				putchar(k + '0');
-				
 				if (i != 7 || j != 8 || k != 9)
 				{
 					putchar(',');
