74505b7c50503732656e81ed179ea4756ac63b8c
ahmed <ahmed@hunter>
2023-03-24 10:57:57 +0200

    solved

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i, j;
+	
+	for (i = 10; i <= 99; i++)
+	{
+		for (j = 10; j <= 99; j++)
+		{
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar(' ');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+			if (i != 99 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
