1ee21bca52b90059b9c7eeab58fbee1fbee0355d
ahmed <ahmed@hunter>
2023-03-24 07:52:11 +0200

    solved

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - the starting point.
+ *
+ * Return: always returning 0
+ */
+
+int main (void)
+{
+	
+	char i = 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		i++;
+	}
+	
+	putchar('\n');
+	return (0);
+}
+
