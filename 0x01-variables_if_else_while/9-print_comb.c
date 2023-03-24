d15e10df0b66ab69e69afb9fc31876407410f999
ahmed <ahmed@hunter>
2023-03-24 08:54:34 +0200

    solved

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *main - the starting point 
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
