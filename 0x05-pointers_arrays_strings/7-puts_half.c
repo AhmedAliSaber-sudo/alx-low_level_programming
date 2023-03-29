fbc39c4cc3b1a80cb7e9b0d34e6255f24b9445b0
ahmed <ahmed@hunter>
2023-03-29 06:45:40 +0200

    7-puts_half.c has been added

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - a function that prints half of a string, followed by a new line.
+ *
+ * @str: pointer varivable.
+ *
+ * Return: void.
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+	while (*str != '\0')
+	{
+		putchar(str[i]);
+		i++;
+		str++;
+	}
+
+	putchar('\n');
+}
