85a408c543867d28a85b2436c85652db83ffd91e
ahmed <ahmed@hunter>
2023-03-29 03:55:08 +0200

    5-rev_string.c has been added

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,7 +14,7 @@ void rev_string(char *s)
 {
 	int i, j;
 	char temp;
-	for (i = 0, j = _strlen(s) - 1; i < j; i++, j--)
+	for (i = 0, j = strlen(s) - 1; i < j; i++, j--)
 	{
 		temp = s[i];
 		s[i] = s[j];
