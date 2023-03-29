aaf9993d2d62121bc03a88eeae89a4878bc2aebc
ahmed <ahmed@hunter>
2023-03-29 07:40:20 +0200

    8-print_array.c has been added

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 -  a function that prints n elements of an array of integers, followed by a new line.
+ *
+ * @a: pointer varivable that point to the first element of the array.
+ *
+ * Return: void.
+ */
+
+void print_array(int *a, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
