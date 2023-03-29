869c450a260fd413142eca01ca0f2c6a13973379
ahmed <ahmed@hunter>
2023-03-29 10:58:05 +0200

    6-puts2.c has been added

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,15 +11,13 @@
 
 void puts2(char *str)
 {
-    int i = 0;
-    
-    while (*str != '\0')
-    {
-        putchar(str[i]);
-        i++;
-        str++;
-    }
+	int i = 0;
+	
+	while (*str != '\0')
+	{
+		putchar(str[i]);
+		i += 2;
+	}
 
-    putchar('\n');
-
+	putchar('\n');
 }
