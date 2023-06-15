43f6378f870f7f8c25ecd451ce7e2f20c6835bcb
Ahmed Ali <54852560+AhmedAliSaber-sudo@users.noreply.github.com>
2023-06-15 09:31:26 +0300

    Update main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,6 +7,13 @@
  * Desc: header file conatin prototypes for all functions used in the 0x08-recursion directory.
  */
 
+int _putchar(char c);
+
+/**
+ * _puts_recursion - print string with new line
+ * @s: string to print
+ */
+
 void _puts_recursion(char *s);
 
 #endif
