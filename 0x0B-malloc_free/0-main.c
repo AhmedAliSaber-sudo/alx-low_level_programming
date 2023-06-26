a3633f732b68068fafbe5ac4f2926ffe5f063845
Ahmed Ali <ahmedewida221510@gmail.com>
2023-06-26 20:35:47 +0300

    update

--- 0x0B-malloc_free/0-main.c
+++ 0x0B-malloc_free/0-main.c
@@ -1,6 +1,7 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+char *create_array(unsigned int, char);
 
 /**
  * simple_print_buffer - prints buffer in hexa
@@ -11,41 +12,43 @@
  */
 void simple_print_buffer(char *buffer, unsigned int size)
 {
-    unsigned int i;
+	unsigned int i;
 
-    i = 0;
-    while (i < size)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", buffer[i]);
-        i++;
-    }
-    printf("\n");
+	i = 0;
+	while (i < size)
+	{
+		if (i % 10)
+		{
+			printf(" ");
+		}
+		if (!(i % 10) && i)
+		{
+			printf("\n");
+		}
+		printf("0x%02x", buffer[i]);
+		i++;
+	}
+	printf("\n");
 }
 
 /**
- * main - check the code for ALX School students.
+ * main - check the code .
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    char *buffer;
+	char *buffer;
+	unsigned int size;
 
-    buffer = create_array(98, 'H');
-    if  (buffer == NULL)
-    {
-        printf("failed to allocate memory\n");
-        return (1);
-    }
-    simple_print_buffer(buffer, 98);
-    free(buffer);
-    return (0);
+	size = 72;
+	buffer = create_array(size, 'S');
+	if (buffer == NULL)
+	{
+		printf("failed to allocate memory\n");
+		return (1);
+	}
+	simple_print_buffer(buffer, size);
+	free(buffer);
+	return (0);
 }
