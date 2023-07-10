53c2ef35869b16f13ce4939756af2b685d397a44
Ahmed Ali <ahmedewida221510@gmail.com>
2023-07-10 16:50:36 +0300

    update

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-	#include <stdlib.h>
-	#include <ctype.h>
-	#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 /**
 * check_num - check str
 * @str: array
