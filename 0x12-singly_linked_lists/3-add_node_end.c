ef092086b3e2448be8a3990e978f3d31662f8a08
Ahmed <ahmedewida221510@gmail.com>
2023-07-28 06:49:30 +0300

    add add_node_end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -25,7 +25,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node->len = strlen(str);
 	new_node->next = NULL;
 
-	if (*head == NULL) 
+	if (*head == NULL)
 		*head = new_node;
 	else
 	{
