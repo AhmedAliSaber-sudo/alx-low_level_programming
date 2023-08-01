0052c1def1aa250f35606dfe16a45833070a4eb8
Ahmed <ahmedewida221510@gmail.com>
2023-08-01 15:19:22 +0300

    fix errors in lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,7 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
-#include <stddef.h>
+
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * struct listint_s - singly linked list.
@@ -12,8 +14,12 @@
 
 typedef struct listint_s
 {
-int n;
-struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
+	/* function prototypes */
 size_t print_listint(const listint_t *h);
+
+
+#endif
