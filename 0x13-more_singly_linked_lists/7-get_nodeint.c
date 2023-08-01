b3e70cc0c71ec16a3403728cd66806ebc0465ae7
Ahmed <ahmedewida221510@gmail.com>
2023-08-01 19:51:32 +0300

    add get_nodeint

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -7,7 +7,7 @@
  * Return: nth node or NULL if failed.
  */
 
-listint_t *get_nodeint_at_index(listint_t *headj, unsigned int index)
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
