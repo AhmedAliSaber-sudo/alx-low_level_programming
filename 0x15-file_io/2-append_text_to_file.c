d16367e62e9ed736d5b534f0e0a0d4ed6be069bc
Ahmed <ahmedewida221510@gmail.com>
2023-08-21 03:34:07 +0300

    enhance the code of append_text_to_file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,16 +9,12 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, n_written, exist;
-
-	exist = access(filename, F_OK);
+	int fd, n_written;
 
 	if (!filename)
 		return (-1);
 
-	if (exist)
-		fd = open(filename, O_WRONLY | O_APPEND);
-
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 
