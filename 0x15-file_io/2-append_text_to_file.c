cfe9b935e89d270b4a8da5a6c0d8d8ba93766f83
Ahmed <ahmedewida221510@gmail.com>
2023-08-21 03:29:56 +0300

    add append_text_to_file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,8 +18,6 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (exist)
 		fd = open(filename, O_WRONLY | O_APPEND);
-	else
-		return (-1);
 
 	if (fd == -1)
 		return (-1);
