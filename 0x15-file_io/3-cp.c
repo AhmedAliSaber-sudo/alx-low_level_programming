2e529cfb83d7262f9fe231f6ad7a6c27e2f5cc8a
Ahmed <ahmedewida221510@gmail.com>
2023-09-05 04:45:16 +0300

    add cp

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,74 @@
+#include "main.h"
+
+#define BUFFER_SIZE 1024
+
+/**
+ * main - the entry point, have the code of copyin one file to another.
+ * @argc: should be 3.
+ * @argv: should have 3 items. first is the program name, the source file,
+ * and the target file.
+ * Return: 0 on sucess, and from 97 to 100 (exit values error).
+ */
+
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, n_read, n_written;
+	char buffer[BUFFER_SIZE];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close(file_from);
+		exit(99);
+	}
+
+	while ((n_read = read(file_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		n_written = write(file_to, buffer, n_read);
+		if (n_written != n_read)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(file_from);
+			close(file_to);
+			exit(99);
+		}
+	}
+
+	if (n_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		close(file_from);
+		close(file_to);
+		exit(98);
+	}
+
+	if (close(file_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		close(file_to);
+		exit(100);
+	}
+
+	if (close(file_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
+		exit(100);
+	}
+
+	return (0);
+}
+
