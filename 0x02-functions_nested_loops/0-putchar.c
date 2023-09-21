c91ef57e3f7daecb7c22cc4dcc6cca6bc3dc0934
Ahmed <ahmedewida221510@gmail.com>
2023-09-21 16:50:46 +0300

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,13 +3,11 @@
 
 /**
  * main - print _putchar
- * @str: an array that hold a stream of chars to make a string.
  * Return: always return 0
  */
 
 int main(void)
 {
-
 	char str[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
 	size_t i;
 
