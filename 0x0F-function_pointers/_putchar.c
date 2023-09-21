5fa34c7a52e9ecd876b84db33b772c72900db0b9
Ahmed <ahmedewida221510@gmail.com>
2023-09-21 16:26:13 +0300

    update

--- 0x0F-function_pointers/_putchar.c
+++ 0x0F-function_pointers/_putchar.c
@@ -2,13 +2,12 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes character c to stdout
- * @c: The character to print
- *
- * Rturn: On success 1.
- * On error , -1 is returned and error is set appropriatelly.
- */
+* _putchar - writes character c to stdout
+* @c: The character to print
+* Return: On success 1.
+* On error , -1 is returned and error is set appropriately.
+*/
 int _putchar(char c)
 {
-        return (write(1, &c, 1));
+		return (write(1, &c, 1));
 }
