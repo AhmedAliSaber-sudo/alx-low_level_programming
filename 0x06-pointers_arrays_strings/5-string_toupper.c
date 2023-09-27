35ce6708dc65859d779593f6db7352f3758bca0d
Ahmed <ahmedewida221510@gmail.com>
2023-09-27 04:29:11 +0300

    update

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* string_toupper - capitalize all letters in string
+* @s: string to manipulate
+* Return: string with all letters capitalized
+*/
+
+char *string_toupper(char *s)
+{
+	while (*s)
+	{
+		if (*s >= 'a' && *s <= 'z')
+		{
+		    *s = *s - 'a' + 'A';
+		}
+		s++;
+	}
+
+}
