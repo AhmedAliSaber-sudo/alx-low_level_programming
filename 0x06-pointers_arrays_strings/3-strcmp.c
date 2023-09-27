bcf17e7f82d3bd5697368fcc878e7d5deb54694c
Ahmed <ahmedewida221510@gmail.com>
2023-09-27 03:54:19 +0300

    update

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _strcmp - compares two strings.
+* @s1: first string.
+* @s2: second string.
+* Return: int that tells num spaces in between
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+
+	while (*s1 || *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+
+		s1++;
+		s2++;
+	}
+		return (0);
+}
