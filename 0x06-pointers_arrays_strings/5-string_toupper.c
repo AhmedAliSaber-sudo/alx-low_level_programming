3f7c79597db17cd6608583d57e6038bb67dfefbb
Ahmed <ahmedewida221510@gmail.com>
2023-09-27 04:46:52 +0300

    update

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,20 +1,21 @@
 #include "main.h"
 
 /**
-* string_toupper - capitalize all letters in string
+* string_toupper -  changes all lowercase letters of a string to uppercase.
 * @s: string to manipulate
 * Return: string with all letters capitalized
 */
 
 char *string_toupper(char *s)
 {
-	while (*s)
-	{
-		if (*s >= 'a' && *s <= 'z')
-		{
-			*s = *s - 'a' + 'A';
-		}
-		s++;
-	}
-}
+	int i;
 
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 'a' + 'A';
+		}
+	}
+	return (s);
+}
